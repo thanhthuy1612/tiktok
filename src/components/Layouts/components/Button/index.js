45e4c13a96374e77a663d77db34ef4bf4a6a4cbf
latiah.bui <latiah.bui@avepoint.com>
2023-02-02 13:48:48 +0700

    update

--- src/components/Layouts/components/Button/index.js
+++ src/components/Layouts/components/Button/index.js
@@ -1,6 +1,21 @@
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
-export default function Button({ to, href, primary, children, onClick, ...passProps }) {
+export default function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    size = 'medium',
+    text = false,
+    disabled = false,
+    rounded = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
     let Comp = 'button';
     let classes = `${styles.wrapper}`;
     const props = {
@@ -13,12 +28,47 @@ export default function Button({ to, href, primary, children, onClick, ...passPr
     } else if (href) {
         props.href = href;
         Comp = 'a';
-    } else if (primary) {
-        classes = `${styles.wrapper} ${styles.primary}`;
+    }
+    if (disabled) {
+        classes = classes.concat(' ', `${styles.disabled}`);
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+    if (primary) {
+        classes = classes.concat(' ', `${styles.primary}`);
+    } else if (outline) {
+        classes = classes.concat(' ', `${styles.outline}`);
+    } else if (text) {
+        classes = classes.concat(' ', `${styles.text}`);
+    }
+    if (rounded) {
+        classes = classes.concat(' ', `${styles.rounded}`);
+    }
+    switch (className) {
+        case 'textColor':
+            classes = classes.concat(' ', `${styles.textColor}`);
+            break;
+        default:
+            break;
+    }
+    switch (size) {
+        case 'small':
+            classes = classes.concat(' ', `${styles.small}`);
+            break;
+        case 'large':
+            classes = classes.concat(' ', `${styles.large}`);
+            break;
+        default:
+            break;
     }
     return (
         <Comp className={classes} {...props}>
-            <span>{children}</span>
+            {leftIcon && <span className={styles.icon}>{leftIcon}</span>}
+            <span className={styles.title}>{children}</span>
+            {rightIcon && <span className={styles.icon}>{rightIcon}</span>}
         </Comp>
     );
 }
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -2,7 +2,7 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faEllipsisVertical, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useRef, useState } from 'react';
 import { Wrapper as PopperWrapper } from '../Popper';
 import SearchAccount from '../SearchAccount';
@@ -11,12 +11,16 @@ import Button from '../Button';
 export default function Header() {
     const [input, setInput] = useState('');
     const [isClose, setIsClose] = useState('0');
+    const [isShow, setIsShow] = useState(false);
     const [searchResult, setSearchResult] = useState([]);
     const searchRef = useRef();
     const handleClose = () => {
         setInput('');
         setIsClose('0');
         searchRef.current.focus();
+    };
+    const handleOpen = () => {
+        setIsShow(!isShow);
     };
     const handleChange = (e) => {
         setInput(e.target.value);
@@ -73,7 +77,24 @@ export default function Header() {
                     </div>
                 </Tippy>
                 <div className={styles.actions}>
+                    <Button text>Upload</Button>
                     <Button primary>Log in</Button>
+                    <Tippy
+                        interactive
+                        visible={isShow}
+                        render={(attrs) => (
+                            <div className={styles.searchResult} tabIndex="-1" {...attrs}>
+                                <PopperWrapper>
+                                    <h4 className={styles.searchTitle}>Accounts</h4>
+                                    <SearchAccount />
+                                </PopperWrapper>
+                            </div>
+                        )}
+                    >
+                        <button className={styles.moreBTN} onClick={handleOpen}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Tippy>
                 </div>
             </div>
         </header>
